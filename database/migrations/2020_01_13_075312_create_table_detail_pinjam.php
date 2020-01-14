358d892f52288210412a8cd06ead330bcc101a85
DellaA <della_andini_27rpl@student.smktelkom-mlg.sch.id>
2020-01-14 13:50:35 +0700

    migrasitable

---
+++ database/migrations/2020_01_13_075312_create_table_detail_pinjam.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableDetailPinjam extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_pinjam', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_pinjam');
+            $table->integer('id_buku');
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_pinjam');
+    }
+}
